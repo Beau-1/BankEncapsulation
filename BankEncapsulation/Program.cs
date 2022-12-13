4d21bda204e3fd2982a78eff4050a9f6f32cf490
Beau <jamesbuchanan0528@gmail.com>
2022-12-13 14:33:25 -0500

    Encap Exercise

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BankEncapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            Console.WriteLine("What is the amount of your deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Deposit successful. " +
+                $"Your current balance is: {account.GetBalance}");
+        }
+    }
+}
